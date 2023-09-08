c9ffcb3f0aa25ced5ca8962d262a3df9ecbb6dd9
solo dev <solodev001@gmail.com>
2023-09-08 23:18:59 +0100

    add javascript code to the project

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert('working')
+const currentDay = document.getElementById('current-day')
+const currentTime = document.getElementById('current-time')
+const userOnlineStatus = document.getElementById('user-online-status')
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+const date = new Date()
+currentDay.innerHTML = weekday[date.getDay()]
+currentTime.innerHTML = date.getUTCMilliseconds()
