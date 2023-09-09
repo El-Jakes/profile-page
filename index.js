98c9a626296629eaabfc9a5ea4f4e4024f067030
ElJakes <jidejacobs1@gmail.com>
2023-09-09 10:53:34 +0100

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("Hello there");
+<<<<<<< HEAD
+=======
+const currentDay = document.getElementById('current-day')
+const currentTime = document.getElementById('current-time')
+const userOnlineStatus = document.getElementById('user-online-status')
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+const date = new Date()
+currentDay.innerHTML = weekday[date.getDay()]
+currentTime.innerHTML = `${date.getUTCMilliseconds()} MS, `
+>>>>>>> 30d9af77e633298ea13f9315ca79a86c24b06132
